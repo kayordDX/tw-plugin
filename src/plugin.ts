86d34b337b439e527170406f134f0be66593db2a
Jaco Kok <kokjaco2@gmail.com>
2023-10-06 13:43:38 +0200

    Common js module

---
+++ src/plugin.ts
@@ -0,0 +1,26 @@
+import plugin from "tailwindcss/plugin";
+import { ConfigOptions } from "./types";
+
+const coreConfig = {
+	theme: {
+		extend: {
+		}
+	}
+};
+
+const kayordPlugin = plugin.withOptions<ConfigOptions>(
+	() => {
+		return ({ addBase, theme }) => {
+			addBase({
+				h1: { fontSize: theme("fontSize.2xl") },
+				h2: { fontSize: theme("fontSize.xl") },
+				h3: { fontSize: theme("fontSize.lg") },
+			});
+		}
+	},
+	() => {
+		return { ...coreConfig };
+	}
+);
+
+export { kayordPlugin }
