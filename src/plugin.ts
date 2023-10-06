2fe62ccdd5f34d2df2c8b535f3efa2b94af673b2
Jaco Kok <kokjaco2@gmail.com>
2023-10-06 14:07:46 +0200

    Additional default config

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,18 @@
 import plugin from "tailwindcss/plugin";
+import { fontFamily } from "tailwindcss/defaultTheme";
 import { ConfigOptions } from "./types";
 
 const coreConfig = {
 	theme: {
 		extend: {
+      borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			fontFamily: {
+				sans: [...fontFamily.sans],
+			},
 		}
 	}
 };
